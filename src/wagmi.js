9cf1f1153ef671d97c83bab7f38cfbe85b5eefd4
DIA <illiybernatskiy@gmail.com>
2025-07-24 18:33:05 +0700

    wagmi config fix

--- src/wagmi.js
+++ src/wagmi.js
@@ -1,18 +1,24 @@
-import { createConfig, http } from "wagmi";
-import { polygon } from "wagmi/chains";
-import { injected } from "@wagmi/connectors";
+import { createConfig, http } from 'wagmi';
+import { polygon } from 'wagmi/chains';
+import { injected } from '@wagmi/connectors';
+import {waitForInjected} from "./waitForInjected";
 
 export const config = createConfig({
     autoConnect: true,
     chains: [polygon],
     connectors: [
+        // Trust Wallet
         injected({
-            target: "trustWallet",
+            target: 'trustWallet',
+            getProvider: () => waitForInjected(),           // ⬅️ новая строка
             shimDisconnect: true,
             UNSTABLE_shimOnConnectSelectAccount: true,
         }),
+
+        // MetaMask
         injected({
-            target: "metaMask",
+            target: 'metaMask',
+            getProvider: () => waitForInjected(),           // ⬅️ новая строка
             shimDisconnect: true,
             UNSTABLE_shimOnConnectSelectAccount: true,
         }),
