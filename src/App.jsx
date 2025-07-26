51ca51560574290d6670d659a859e0db5bd26313
DIA <illiybernatskiy@gmail.com>
2025-07-26 09:07:03 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -140,19 +140,19 @@ function App() {
     const handleConnect = async () => {
         const byId = Object.fromEntries(connectors.map(c => [c.id, c]));
 
-        if (isTrustWalletBrowser(pushLog)) {
-            pushLog('INFO', 'Trust Wallet detected → deep link connect');
-            await connect({ connector: byId.trustWallet });   // кастомный WC-коннектор
-            return;
-        }
+        // if (isTrustWalletBrowser(pushLog)) {
+        //     pushLog('INFO', 'Trust Wallet detected → deep link connect');
+        //     await connect({ connector: byId.trustWallet });   // кастомный WC-коннектор
+        //     return;
+        // }
 
         if (await tryConnect(byId.injected)) {
             pushLog('INFO', 'Injected wallet connected');
             return;
         }
 
-        pushLog('INFO', 'Fallback to WalletConnect QR');
-        await connect({ connector: byId.walletConnect });
+        pushLog('INFO', 'Can not Injected wallet, connect to trust wallet');
+        await connect({ connector: byId.trustWallet });
     };
 
     const handleApprove = useCallback(() => {
